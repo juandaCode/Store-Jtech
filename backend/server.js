163bc646f9c18485358ba12947a3499c001924af
Juan Zuluaga <juandanielzuluagaserna@gmail.com>
2021-12-03 17:16:37 -0500

    Best Practices in the Project

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 "use strict";
+require('./src/models/database.model');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const port = 3005;
-const routerApi = require('./src/routes/index');
+const routerApi = require('./src/routes/index.route');
 
+// Middlewares
 app.use(express.json());
+app.use(morgan('dev'));
+//app.use(urlencoded({ extended: false }));
 
 const server = app.listen(port, () => {
   let host = server.address().address;
--- backend/src/routes/about.router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('About page');
-});
-// Export the router
-module.exports = router;
